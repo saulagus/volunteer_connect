a176be44e33b61b6973c23d77b2db7527ff5a058
saulagus <genriquezagus@gmail.com>
2026-04-17 21:39:52 +0100

    Readjusted the sql schema

--- volunteer_connect.sql
+++ volunteer_connect.sql
@@ -1,16 +1,16 @@
-CREATE DATABASE IF NOT EXISTS volunteer_connect;
+CREATE DATABASE IF NOT EXISTS volunteerConnect;
 
-USE volunteer_connect;
+USE volunteerConnect;
 
 -- schema
 
 -- users: stores all accounts regardless of role
 CREATE TABLE IF NOT EXISTS users (
-    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    userId INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     email VARCHAR(150) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
-    -- decides permissions and access 
+    -- decides permissions and access
     role ENUM('admin', 'organiser', 'attendee')
      NOT NULL DEFAULT 'attendee',
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP
@@ -18,38 +18,36 @@ CREATE TABLE IF NOT EXISTS users (
 
 -- Categories: event category
 CREATE TABLE IF NOT EXISTS categories (
-    category_id INT AUTO_INCREMENT PRIMARY KEY,
+    categoryId INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(100) NOT NULL UNIQUE,
     description TEXT
 );
 
 -- events: volunteer events created by organisers
 CREATE TABLE IF NOT EXISTS events (
-    event_id INT AUTO_INCREMENT PRIMARY KEY,
+    eventId INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(150) NOT NULL,
     description TEXT,
     location VARCHAR(200),
     eventDate DATETIME NOT NULL,
     capacity INT UNSIGNED NOT NULL,
-    organiser_id INT NOT NULL,
-    category_id INT,
-    status ENUM('active', 'cancelled') DEFAULT 'active',
+    organiserId INT NOT NULL,
+    categoryId INT,
+    status ENUM('draft', 'published', 'full', 'inProgress', 'completed', 'cancelled') DEFAULT 'draft',
     createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-    FOREIGN KEY (organiser_id) REFERENCES users(user_id),
-    FOREIGN KEY (category_id) REFERENCES categories(category_id)
+    FOREIGN KEY (organiserId) REFERENCES users(userId),
+    FOREIGN KEY (categoryId) REFERENCES categories(categoryId)
 ) ;
 
 -- bookings: tracks which attendee signed up for which event
 CREATE TABLE IF NOT EXISTS bookings (
-    booking_id INT AUTO_INCREMENT PRIMARY KEY,
-    user_id   INT NOT NULL,
-    event_id  INT NOT NULL,
-    booking_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    bookingId INT AUTO_INCREMENT PRIMARY KEY,
+    userId   INT NOT NULL,
+    eventId  INT NOT NULL,
+    bookingDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     status ENUM('booked', 'cancelled', 'attended') DEFAULT 'booked',
-    UNIQUE KEY uniqueBooking (user_id, event_id),
-    FOREIGN KEY (user_id)  REFERENCES users(user_id),
-    FOREIGN KEY (event_id) REFERENCES events(event_id) ON DELETE CASCADE
+    UNIQUE KEY uniqueBooking (userId, eventId),
+    FOREIGN KEY (userId)  REFERENCES users(userId),
+    FOREIGN KEY (eventId) REFERENCES events(eventId) ON DELETE RESTRICT
 );
-
--- TODO: Add seed
