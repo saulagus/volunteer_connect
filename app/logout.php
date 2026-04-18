dbb25a1658767f31a25039f5e447532a140c4304
saulagus <genriquezagus@gmail.com>
2026-04-18 15:52:10 +0100

    Add logout.php

---
+++ app/logout.php
@@ -0,0 +1,27 @@
+<?php
+// file to destroy the session and log user out
+
+session_start();
+
+// clear all session data on the server side
+$_SESSION = [];
+
+// remove the session cookie from the browser so there's no leftover cookie
+if (ini_get('session.use_cookies')) {
+    $params = session_get_cookie_params();
+    // delete cookie
+    setcookie(
+        // use the same data as original cookie
+        session_name(), '',
+        // set expiration in the past
+        time() - 3600,
+        $params['path'], $params['domain'],
+        $params['secure'], $params['httponly']
+    );
+}
+
+// destroy the session on the server
+session_destroy();
+// redirect to login 
+header('Location: /login.php');
+exit();
